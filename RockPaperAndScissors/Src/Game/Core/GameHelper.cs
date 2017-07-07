2bf2c0d3394eaa31f22a09bd1561bcb8509908b0
Allan Douglas <allandouglas@live.com>
2017-07-07 00:37:03 -0300

    Improvements in the comparison method

--- RockPaperAndScissors/Src/Game/Core/GameHelper.cs
+++ RockPaperAndScissors/Src/Game/Core/GameHelper.cs
@@ -14,23 +14,30 @@ namespace RockPaperAndScissors.Src.Game.Core
         /// Compare the selec
         /// </summary>
         /// <param name="one"></param>
-        /// <param name="Two"></param>
-        static IPlayer CompareWeapons(IPlayer one, IPlayer Two)
+        /// <param name="two"></param>
+        internal static IPlayer CompareWeapons(IPlayer one, IPlayer two)
         {
+
+            // check about null weapons
+            if (one.SelectedWeapon == null | two.SelectedWeapon == null)
+            {
+                throw new NullReferenceException("Both players must have non-null weapons");
+            }
+
             // check if Selected Weapons are the same
             // return null
-            if (one.SelectedWeapon == Two.SelectedWeapon)
+            if ( one.SelectedWeapon.GetType().Equals(two.SelectedWeapon.GetType()))
             {
                 return null;
             }
 
             // check if the Two's Weapon is more strong the One's Weapon 
             // return Two
-            foreach(var weapon in Two.SelectedWeapon.Weaknesses)
+            foreach(IWeakness weakness in one.SelectedWeapon.Weaknesses)
             {
-                if(weapon == one.SelectedWeapon)
+                if(weakness.Weapon.GetType().Equals(two.SelectedWeapon.GetType()))
                 {
-                    return Two;
+                    return two;
                 }
             }
 
